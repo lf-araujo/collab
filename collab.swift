7fbe8a102a834a9ab75a7a68a37c58b0de0d7dfc
luis <luis@localhost.localdomain>
2018-01-10 16:34:00 +1100

    Completed first stage of the software

--- collab.swift
+++ collab.swift
@@ -4,6 +4,12 @@ import Files // marathon: https://github.com/JohnSundell/Files.git
 import Foundation
 
 /* 
+    TODOs
+
+    - [X] If @email.com, then unshuffle
+
+    ---
+    
     This is a marathon swift script / swift code that can be used to shuffle 
     authors, anonymizing  them so to allow emailing the other participants
     with their colleagues material. This is a quasi-peer-review tool for use
@@ -27,9 +33,6 @@ import Foundation
     1. Author: the student who uploaded the original;
     2. Reviewer: whoever is making suggestions in a text that is not his own.
 
-    TODOs
-
-    - [ ] If @email.com, then unshuffle
 */
 
 extension MutableCollection {
@@ -55,17 +58,23 @@ extension Sequence {
     }
 }
 
+extension Dictionary where Value: Equatable {
+    func allKeys(forValue val: Value) -> [Key] {
+        return self.filter { $1 == val }.map { $0.0 }
+    }
+}
+
 
 func original() {
     var email: String = ""
     var document: [String] = []
-    var emaillist:[String] = []
+    var emaillist: [String] = []
 
     for file in Folder.current.files {
         guard file.extension == "md" else {
             continue
         }
-
+ 
         let content = try! file.readAsString()
         let pattern = "(?s)(?<=---).*(?=---)"
         if let range = content.range(of: pattern, options: .regularExpression) {
@@ -75,8 +84,8 @@ func original() {
             let author = value["author"].string!
             let emailline = "email: " + email
             let authorline = "author: " + author
-            var anonymcontent = content.replacingOccurrences(of: "\n\(emailline)", with: "")
-            anonymcontent = anonymcontent.replacingOccurrences(of: "\n\(authorline)", with: "")
+            var anonymcontent = content.replacingOccurrences(of: "\n\(emailline)", with: "\nemail: ")
+            anonymcontent = anonymcontent.replacingOccurrences(of: "\n\(authorline)", with: "\nauthor: ")
 
             document.append(anonymcontent)
         }
@@ -100,13 +109,56 @@ func original() {
 
 
 func edited() {
-    print("Estou no edited")
+    var originalpairs: [String: String] = [:]
+    var email: String = ""
+    var author: String = ""
+    var document: [String] = []
+    
+    // listing the original pairs
+    for  file in Folder.current.files {
+        guard file.extension == "md" else {
+            continue
+        }
+ 
+        let content = try! file.readAsString()
+        let pattern = "(?s)(?<=---).*(?=---)"
+        if let range = content.range(of: pattern, options: .regularExpression) {
+            let text = String(content[range])
+            let value = try! Yaml.load(text)
+            email = value["email"].string!
+            author = value["author"].string!
+        }   
+        originalpairs[author] = email
+    }
+
+    // reverting shuffle
+    for file in Folder.current.files {
+        guard file.name.contains("@") && !file.name.contains("edited_") else {
+            continue
+        }
+
+        let content = try! file.readAsString()
+        let pattern = "(?s)(?<=---).*(?=---)"
+        if let range = content.range(of: pattern, options: .regularExpression) {
+            let text = String(content[range])
+            let value = try! Yaml.load(text)
+            email = file.name
+            // the following is a bit tricky, it is reducing a dict into string with .reduce()
+            author = originalpairs.allKeys(forValue: email).reduce("") { $0 + String($1) }
+            var editedcontent = text.replacingOccurrences(of: "email: ", with: "email: \(email)")
+            editedcontent = editedcontent.replacingOccurrences(of: "author: ", with: "author: \(author)")
+            editedcontent = content.replacingOccurrences(of: text, with: editedcontent)
+
+            let outfile = try! Folder.current.createFile(named: "edited_\(email)")
+            try! outfile.write(string: editedcontent)
+        }   
+
+    }
 }
 
-// preciso pegar a lista de arquivos e checar com range se tem algum com arroba
 
 let pattern = VerEx().any("@")
-var allfiles:String = ""
+var allfiles: String = ""
 
 for file in  Folder.current.files {
     allfiles += file.name
