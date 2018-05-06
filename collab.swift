4abc5af84d9d9404996c1a001d0570ab66cb3ea4
luis <luis@localhost.localdomain>
2018-05-06 08:59:26 -0300

    resolved TODOs

--- collab.swift
+++ collab.swift
@@ -1,12 +1,13 @@
-import VerbalExpressions // marathon: https://github.com/VerbalExpressions/SwiftVerbalExpressions.git
+import Foundation
 import Yaml // marathon: https://github.com/behrang/YamlSwift.git
 import Files // marathon: https://github.com/JohnSundell/Files.git
-import Foundation
 
 /* 
     TODO
 
-    - [ ] Create a yank function, that extracts btw ---
+    - [X] Create a yank function, that extracts btw ---
+    - [X] Remove all force unwraps (!) as an exercise
+    - [X] Remove VerbalExpressions dependency, for coherence
     
     This is a marathon swift script / swift. code that can be used to shuffle 
     authors, anonymizing  them so to allow emailing the other participants
@@ -40,7 +41,7 @@ extension MutableCollection {
         guard c > 1 else { return }
 
         for (firstUnshuffled, unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
-            let d: IndexDistance = numericCast(Int(random() % numericCast(unshuffledCount) ))
+            let d: Int = numericCast(Int(random() % numericCast(unshuffledCount) ))
             let i = index(firstUnshuffled, offsetBy: d)
             swapAt(firstUnshuffled, i)
         }
@@ -62,6 +63,10 @@ extension Dictionary where Value: Equatable {
     }
 }
 
+func yankBlock( content:String ) -> String? {
+    let range = content.range(of: "(?s)(?<=---).*(?=---)", options: .regularExpression)
+    return String(content[range!])
+}
 
 func original() {
     var email: String = ""
@@ -72,61 +77,67 @@ func original() {
         guard file.extension == "md" else {
             continue
         }
- 
-        let content = try! file.readAsString()
-        let pattern = "(?s)(?<=---).*(?=---)"
-        if let range = content.range(of: pattern, options: .regularExpression) {
-            let text = String(content[range])
-            let value = try! Yaml.load(text)
-            email = value["email"].string!
-            let author = value["author"].string!
-            let emailline = "email: " + email
-            let authorline = "author: " + author
-            var anonymcontent = content.replacingOccurrences(of: "\n\(emailline)", with: "\nemail: ")
-            anonymcontent = anonymcontent.replacingOccurrences(of: "\n\(authorline)", with: "\nauthor: ")
 
-            document.append(anonymcontent)
+        guard let content = try? file.readAsString() else {
+            print("Couldn't read original file.")
+            return
         }
 
-        
+        let text = yankBlock(content: content)
+        guard let value = try? Yaml.load(text!) else {
+            print("Couldn't read yaml.")
+            return
+        }
+        email = value["email"].string!
+        let author = value["author"].string!
+        let emailline = "email: " + email
+        let authorline = "author: " + author
+        var anonymcontent = content.replacingOccurrences(of: "\n\(emailline)", with: "\nemail: ")
+        anonymcontent = anonymcontent.replacingOccurrences(of: "\n\(authorline)", with: "\nauthor: ")
+        document.append(anonymcontent)     
         emaillist.append(email)
     }
 
     emaillist = emaillist.shuffled()
-
 
     for (index, file) in Folder.current.files.enumerated() {
         guard file.extension == "md" else {
             continue
         }
 
-        let outfile = try! Folder.current.createFile(named: emaillist[index])
-        try! outfile.write(string: document[index])
-    } 
+        guard let outfile = try? Folder.current.createFile(named: emaillist[index]) else {
+            print("Couldn't create outfile.")
+            return
+        }
+
+        try? outfile.write(string: document[index])
+
+    }
 }
-
-
 
 func edited() {
     var originalpairs: [String: String] = [:]
     var email: String = ""
     var author: String = ""
-    var document: [String] = []
-    
+
     // listing the original pairs
     for  file in Folder.current.files {
         guard file.extension == "md" else {
             continue
         }
- 
-        let content = try! file.readAsString()
-        let pattern = "(?s)(?<=---).*(?=---)"
-        if let range = content.range(of: pattern, options: .regularExpression) {
-            let text = String(content[range])
-            let value = try! Yaml.load(text)
-            email = value["email"].string!
-            author = value["author"].string!
-        }   
+
+        guard let content = try? file.readAsString() else {
+            print("Couldn't read edited file.")
+            return
+        }
+        let text = yankBlock(content: content)
+        guard let value = try? Yaml.load(text!) else {
+            print("Couldn't read yaml of edited file.")
+            return
+        }
+        email = value["email"].string!
+        author = value["author"].string!
+
         originalpairs[author] = email
     }
 
@@ -136,34 +147,36 @@ func edited() {
             continue
         }
 
-        let content = try! file.readAsString()
-        let pattern = "(?s)(?<=---).*(?=---)"
-        if let range = content.range(of: pattern, options: .regularExpression) {
-            let text = String(content[range])
-            let value = try! Yaml.load(text)
-            email = file.name
-            // the following is a bit tricky, it is reducing a dict into string with .reduce()
-            author = originalpairs.allKeys(forValue: email).reduce("") { $0 + String($1) }
-            var editedcontent = text.replacingOccurrences(of: "email: ", with: "email: \(email)")
-            editedcontent = editedcontent.replacingOccurrences(of: "author: ", with: "author: \(author)")
-            editedcontent = content.replacingOccurrences(of: text, with: editedcontent)
+        guard let content = try? file.readAsString() else {
+            print("Couldn't read edited file.")
+            return
+        }
+        let text = yankBlock(content: content)!
+        email = file.name
 
-            let outfile = try! Folder.current.createFile(named: "edited_\(email)")
-            try! outfile.write(string: editedcontent)
-        }   
+        // the following is a bit tricky, it is reducing a dict into string with .reduce()
+        author = originalpairs.allKeys(forValue: email).reduce("") { $0 + String($1) }
+        var editedcontent = text.replacingOccurrences(of: "email: ", with: "email: \(email)")
+        editedcontent = editedcontent.replacingOccurrences(of: "author: ", with: "author: \(author)")
+        editedcontent = content.replacingOccurrences(of: text, with: editedcontent)
 
+        // writing the outfile
+        guard let outfile = try? Folder.current.createFile(named: "edited_\(email)") else {
+            print("Couldn't create outfile.")
+            return
+        }
+        try? outfile.write(string: editedcontent)
     }
 }
 
-
-let pattern = VerEx().any("@")
 var allfiles: String = ""
 
 for file in  Folder.current.files {
     allfiles += file.name
 }
 
-if pattern.test(allfiles) {
+
+if allfiles.range(of:"@") != nil {
     edited()
 } else {
     original()
